1e36c070415201a56219e60e54599f8cdf782715
Ernest Cui <ecuiships@126.com>
2023-06-07 22:56:44 +0800

    chore: separate header only and header+payload functions

--- utils.py
+++ utils.py
@@ -4,7 +4,26 @@ import numpy as np
 
 
 def get_54_header(pkt: Packet) -> np.ndarray:
-    SLICE_LENGTH = 100
+    TCP_SLICE_LENGTH = 54
+    UDP_SLICE_LENGTH = 42
+
+    if TCP in pkt:
+        if len(raw(pkt)) < TCP_SLICE_LENGTH:
+            raise RuntimeError(f"TCP packet size < {TCP_SLICE_LENGTH}")
+        return np.array(np.frombuffer(raw(pkt)[:TCP_SLICE_LENGTH], dtype=np.uint8))
+    elif UDP in pkt:
+        if len(raw(pkt)) < UDP_SLICE_LENGTH:
+            raise RuntimeError(f"UDP packet size < {UDP_SLICE_LENGTH}")
+        # Pad UDP with trailing zeros to make it 54-byte long
+        return np.concatenate([
+            np.frombuffer(raw(pkt)[:UDP_SLICE_LENGTH], dtype=np.uint8),
+            np.zeros(TCP_SLICE_LENGTH - UDP_SLICE_LENGTH, dtype=np.uint8)])
+    else:
+        raise RuntimeError("packet is neither TCP nor UDP")
+
+
+def get_header_payload(pkt: Packet) -> np.ndarray:
+    SLICE_LENGTH = 150
 
     if TCP in pkt or UDP in pkt:
         if len(raw(pkt)) >= SLICE_LENGTH:
@@ -12,7 +31,7 @@ def get_54_header(pkt: Packet) -> np.ndarray:
         # Pad with trailing zeros
         return np.concatenate([
             np.frombuffer(raw(pkt), dtype=np.uint8),
-            np.zeros(SLICE_LENGTH-len(raw(pkt)), dtype=np.uint8)])
+            np.zeros(SLICE_LENGTH - len(raw(pkt)), dtype=np.uint8)])
     else:
         raise RuntimeError("packet is neither TCP nor UDP")
 
@@ -23,9 +42,9 @@ def get_tcp_udp_headers(packets: PacketList) -> np.ndarray:
     if len(tcp_udp_packets) == 0:
         return np.array([])
 
-    result: np.ndarray = np.array([get_54_header(tcp_udp_packets[0])])
+    result: np.ndarray = np.array([get_header_payload(tcp_udp_packets[0])])
 
     for pkt in tcp_udp_packets[1:]:
-        result = np.concatenate([result, np.array([get_54_header(pkt)])])
+        result = np.concatenate([result, np.array([get_header_payload(pkt)])])
 
     return result
