2f3bb6434e52760f4486ceeb47d18bff415c57d5
Ernest Cui <ecuiships@126.com>
2023-06-07 22:03:07 +0800

    refactor: header only -> header+payload

--- utils.py
+++ utils.py
@@ -4,20 +4,15 @@ import numpy as np
 
 
 def get_54_header(pkt: Packet) -> np.ndarray:
-    TCP_SLICE_LENGTH = 54
-    UDP_SLICE_LENGTH = 42
+    SLICE_LENGTH = 100
 
-    if TCP in pkt:
-        if len(raw(pkt)) < TCP_SLICE_LENGTH:
-            raise RuntimeError(f"TCP packet size < {TCP_SLICE_LENGTH}")
-        return np.array(np.frombuffer(raw(pkt)[:TCP_SLICE_LENGTH], dtype=np.uint8))
-    elif UDP in pkt:
-        if len(raw(pkt)) < UDP_SLICE_LENGTH:
-            raise RuntimeError(f"UDP packet size < {UDP_SLICE_LENGTH}")
-        # Pad UDP with trailing zeros to make it 54-byte long
+    if TCP in pkt or UDP in pkt:
+        if len(raw(pkt)) >= SLICE_LENGTH:
+            return np.array(np.frombuffer(raw(pkt)[:SLICE_LENGTH], dtype=np.uint8))
+        # Pad with trailing zeros
         return np.concatenate([
-            np.frombuffer(raw(pkt)[:UDP_SLICE_LENGTH], dtype=np.uint8),
-            np.zeros(TCP_SLICE_LENGTH-UDP_SLICE_LENGTH, dtype=np.uint8)])
+            np.frombuffer(raw(pkt), dtype=np.uint8),
+            np.zeros(SLICE_LENGTH-len(raw(pkt)), dtype=np.uint8)])
     else:
         raise RuntimeError("packet is neither TCP nor UDP")
 
